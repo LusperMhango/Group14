bbed3825a5fc8f1805eead832a2082543cba544b
jombo-william <168138622+jombo-william@users.noreply.github.com>
2024-11-03 03:09:11 -0800

    updates on logout

--- automated/src/components/logOut.js
+++ automated/src/components/logOut.js
@@ -52,12 +52,12 @@ function Logout() {
               <li><button onClick={() => navigate('/page1')} className="text-gray-900 hover:underline">Page 1</button></li>
               <li><button onClick={() => navigate('/page2')} className="text-gray-900 hover:underline">Page 2</button></li>
               <li><button onClick={() => navigate('/page3')} className="text-gray-900 hover:underline">Page 3</button></li>
-              <li><button onClick={() => navigate('/page4')} className="text-gray-900 hover:underline">Page 4</button></li>
+              {/* <li><button onClick={() => navigate('/page4')} className="text-gray-900 hover:underline">Page 4</button></li>
               <li><button onClick={() => navigate('/page5')} className="text-gray-900 hover:underline">Page 5</button></li>
               <li><button onClick={() => navigate('/page6')} className="text-gray-900 hover:underline">Page 6</button></li>
               <li><button onClick={() => navigate('/page7')} className="text-gray-900 hover:underline">Page 7</button></li>
               <li><button onClick={() => navigate('/page8')} className="text-gray-900 hover:underline">Page 8</button></li>
-              <li><button onClick={() => navigate('/page9')} className="text-gray-900 hover:underline">Page 9</button></li>
+              <li><button onClick={() => navigate('/page9')} className="text-gray-900 hover:underline">Page 9</button></li> */}
             </ul>
           
         )}
