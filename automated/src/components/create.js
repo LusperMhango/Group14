ab454c150d0dfbc576617ab8bc318918f64f588d
Victoria Misomali <Bsc-inf-03-19@unima.ac.mw>
2024-11-11 19:21:35 -0800

    added some staff

--- automated/src/components/create.js
+++ automated/src/components/create.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const CreateAccount = () => {
+const Create = () => {
   const navigate = useNavigate();
 
   return (
@@ -94,4 +94,4 @@ const CreateAccount = () => {
   );
 };
 
-export default CreateAccount;
+export default Create;
