dacfb4ffc4cb1af8a9a79622ea1de94da9a25e34
jombo-william <168138622+jombo-william@users.noreply.github.com>
2024-11-22 19:06:37 -0800

    correction of errors in nav bar and loan amount

--- src/components/LoanAmount.js
+++ src/components/LoanAmount.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
-import { useNavigate } from "react-router-dom";
+
 
 const ApprovedLoanDetails = () => {
   const navigate = useNavigate();
